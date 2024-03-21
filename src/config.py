3ebfec86d862e87643d7b536ab75a44405eb9743
helenyukuo <helenyukuo@uchicago.edu>
2024-03-21 00:54:51 -0500

    Your commit message

--- src/config.py
+++ src/config.py
@@ -1,11 +1,20 @@
+"""
+This configuration module sets up the directory structure and global settings for the project.
+It defines the base, data, and output directories, as well as key date parameters for the analysis.
+"""
 from decouple import config
 from pathlib import Path
-
+ 
 BASE_DIR = Path(__file__).parent.parent
 
 DATA_DIR = config('DATA_DIR', default=BASE_DIR / 'data/', cast=Path)
 OUTPUT_DIR = config('OUTPUT_DIR', default=BASE_DIR / 'output/', cast=Path)
-WRDS_USERNAME = config("WRDS_USERNAME", default="")
+
+USE_BBG = False
+START_DT = "1988-01-29"
+PAPER_END_DT = "2017-06-30"
+CURR_END_DT = "2021-08-31"
+
 
 if __name__ == "__main__":
     
@@ -17,3 +26,4 @@ if __name__ == "__main__":
     # (DATA_DIR / 'derived').mkdir(parents=True, exist_ok=True)
 
     OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+    
